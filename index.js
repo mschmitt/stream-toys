ced41dc9c153263d40c1758a0bc173f9ca086bea
Martin Schmitt 🐙 <mas@scsy.de>
2024-10-20 21:51:02 +0200

    Non-configurable prototype incl. dependencies

---
+++ index.js
@@ -0,0 +1,27 @@
+var seconds = 300;
+const chars = ['9','8','7','6','5','4','3','2','1','0'];
+$(document).ready(function() {
+	$('#countdown_mins').flapper({width: 2, chars: chars});
+	$('#countdown_colon').flapper({width: 1, chars: [':']});
+	$('#countdown_secs').flapper({width: 2, chars: chars});
+	show_countdown(seconds);
+	setInterval(function() {
+		seconds = seconds - 1;
+		show_countdown(seconds);
+	}, 1000);
+});
+
+function show_countdown(seconds) {
+	var split_mins = Math.floor(seconds/60);
+	var split_secs = seconds % 60;
+	if (split_mins < 10){
+		split_mins = '0' + split_mins;
+	}
+	if (split_secs < 10){
+		split_secs = '0' + split_secs;
+	}
+	$('#countdown_mins').val(split_mins).change();
+	$('#countdown_colon').val(':').change();
+	$('#countdown_secs').val(split_secs).change();
+}
+
