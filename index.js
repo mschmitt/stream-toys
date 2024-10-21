d74f2f9cc0d760b4bbbd675076608a4ac2c48b60
Martin Schmitt <mas@scsy.de>
2024-10-21 08:05:09 +0000

    Clear Interval - Does this work?

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ $(document).ready(function() {
 	$('#countdown_colon').flapper({width: 1, chars: [':']});
 	$('#countdown_secs').flapper({width: 2, chars: chars});
 	show_countdown(seconds);
-	setInterval(function() {
+	const flap_interval = setInterval(function() {
 		seconds = seconds - 1;
 		show_countdown(seconds);
 	}, 1000);
 });
 
 function show_countdown(seconds) {
+	if (seconds < 0) {
+		clearInterval(flap_interval);
+		return;
+	}
 	var split_mins = Math.floor(seconds/60);
 	var split_secs = seconds % 60;
 	if (split_mins < 10){
