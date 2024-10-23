98c56b793e3bfecab81af379e017b8a960d32ab4
Martin Schmitt <mas@scsy.de>
2024-10-23 19:39:56 +0200

    Animation still stuck, try to unwedge, add random glitch effect

--- clock-minutes.js
+++ clock-minutes.js
@@ -1,11 +1,11 @@
-const chars = ['0','1','2','3','4','5','6','7','8','9'];
+const chars = ['0','1','2','3','4','5','6','7','8','9',' '];
 $(document).ready(function() {
 	$('#clock_hrs').flapper({width: 2, chars: chars});
 	$('#clock_colon').flapper({width: 1, chars: [':']});
 	$('#clock_mins').flapper({width: 2, chars: chars});
 	setInterval(function() {
 		show_clock();
-	}, 500);
+	}, 1000); // Note we're on stream, viewers clock will always be offset.
 });
 
 function show_clock() {
@@ -14,9 +14,19 @@ function show_clock() {
 	var minutes = date.getMinutes();
 	if (hours < 10){
 		hours = '0' + hours;
+	}else{
+		hours = hours.toString();
 	}
 	if (minutes < 10){
 		minutes = '0' + minutes;
+	}else{
+		minutes = minutes.toString();
+	}
+	// Pretend to glitch randomly every once in a while
+	if (Math.floor(Math.random() * 200) == 0) {
+		$('#clock_hrs').val('00').change();
+		$('#clock_mins').val('00').change();
+		return;
 	}
 	$('#clock_hrs').val(hours).change();
 	$('#clock_colon').val(':').change();
